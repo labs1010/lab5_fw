7cda5289053b18c849dd3df487441881ff1cd706
labs1010 <fedoriuk.mircha@chnu.edu.ua>
2025-11-04 12:20:17 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,3 @@ export default defineConfig({
   plugins: [react()],
   base: '/lab5_fw/',
 })
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/lab5_fw/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-  },
-})
